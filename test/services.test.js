834372b99d61b6654e3098ef0406748240523046
Tim Thompson <scubatim84@gmail.com>
2022-05-01 15:46:06 -0600

    Test CI passing

--- test/services.test.js
+++ test/services.test.js
@@ -3,7 +3,7 @@ import chai from 'chai';
 const expect = chai.expect;
 
 describe('Sample unit tests', () => {
-    it('should fail', () => {
-        expect(1 + 1).to.equal(3);
+    it('Sample test', () => {
+        expect(1 + 1).to.equal(2);
     })
 })
