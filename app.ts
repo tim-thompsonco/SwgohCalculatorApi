cf679e7e0290a1dd57ca92aebb1e9b85f03906da
Tim Thompson <scubatim84@gmail.com>
2022-05-07 09:27:04 -0600

    Cleanup

--- app.ts
+++ app.ts
@@ -7,15 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const client = createClient({ url: process.env.REDIS_URL });
-
-const testRedis = async () => {
-  await client.connect();
-  await client.set('authToken', 'test');
-  const value = await client.get('authToken');
-  console.log(value);
-};
-
-testRedis();
+await client.connect();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
