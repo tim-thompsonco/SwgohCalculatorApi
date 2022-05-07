995081834ca3d73ff9f045d2dd055a0489fed0d6
Tim Thompson <scubatim84@gmail.com>
2022-05-07 10:20:10 -0600

    Configure data source to use Redis cache for auth token

--- data-sources/swgoh-help-api.ts
+++ data-sources/swgoh-help-api.ts
@@ -1,12 +1,16 @@
+/* eslint-disable camelcase */
 import axios from 'axios';
 import url from 'url';
 
+const SWGOH_HELP_AUTH_TOKEN = 'authToken';
+
+// We have to use snake case for the API request because that's what SWGOH Help uses
 const authParams = new url.URLSearchParams({
   username: process.env.SWGOH_HELP_USERNAME,
   password: process.env.SWGOH_HELP_PASSWORD,
-  grantType: process.env.SWGOH_HELP_GRANT_TYPE,
-  clientId: process.env.SWGOH_HELP_CLIENT_ID,
-  clientSecret: process.env.SWGOH_HELP_CLIENT_SECRET
+  grant_type: process.env.SWGOH_HELP_GRANT_TYPE,
+  client_id: process.env.SWGOH_HELP_CLIENT_ID,
+  client_secret: process.env.SWGOH_HELP_CLIENT_SECRET
 });
 
 const authHeaders = {
@@ -15,8 +19,14 @@ const authHeaders = {
   }
 };
 
-export const getSwgohHelpApiAuthToken = async (): Promise<string> => {
-  const response = await axios.post('https://api.swgoh.help/auth/signin', authParams, authHeaders);
+export const getSwgohHelpApiAuthToken = async (client): Promise<string> => {
+  let authToken = await client.get(SWGOH_HELP_AUTH_TOKEN);
 
-  return response.data?.access_token;
+  if (!authToken) {
+    const response = await axios.post('https://api.swgoh.help/auth/signin', authParams, authHeaders);
+    authToken = response.data?.access_token;
+    await client.set(SWGOH_HELP_AUTH_TOKEN, authToken, { EX: response.data?.expires_in });
+  }
+
+  return authToken;
 };
