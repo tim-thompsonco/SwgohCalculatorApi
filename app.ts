da4ad94801e5056ee66dc2d04be93324cf5e7213
Tim Thompson <scubatim84@gmail.com>
2022-05-07 09:04:37 -0600

    Set up Redis

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 import 'dotenv/config';
 
 import express from 'express';
-
-import { getSwgohHelpApiAuthToken } from './data-sources/swgoh-help-api';
+import { createClient } from 'redis';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const client = createClient({ url: process.env.REDIS_URL });
+
+const testRedis = async () => {
+  await client.connect();
+  await client.set('authToken', 'test');
+  const value = await client.get('authToken');
+  console.log(value);
+};
+
+testRedis();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
